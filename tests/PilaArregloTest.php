ae2d8cec9c26c974614781f61fe36856a5cc46fc
mmartine2008 <mariano.andres.martinez@gmail.com>
2024-04-26 13:13:15 -0300

    Testeo con datos pila fijos

--- tests/PilaArregloTest.php
+++ tests/PilaArregloTest.php
@@ -10,13 +10,48 @@ class PilaArregloTest extends TestCase {
 
     public function testApilar() {
         $p = new PilaArreglo();
-        
         $value = 5;
-
         $p->Apilar($value);
 
         $this->assertFalse($p->PilaVacia());
         $this->assertEquals($p->Tope(), $value);
-        
     }
+
+    public function testDesapilar() {
+        $p = new PilaArreglo();
+        $value = 5;
+        $p->Apilar($value);
+
+        $p->Desapilar();
+        $this->assertTrue($p->PilaVacia());
+    }
+
+    /**
+     * @dataProvider pilaProvider
+     */
+    public function testApilar2($datos, $tamanio) {
+        $p = new PilaArreglo();
+        $cantidad = 0;
+
+        foreach ($datos as $value) {
+            $p->Apilar($value);
+        }
+
+        while (!$p->PilaVacia()) {
+            $p->Desapilar();
+            $cantidad ++;
+        }
+
+        $this->assertTrue($p->PilaVacia());
+        $this->assertEquals($cantidad, $tamanio);
+    }
+
+    public function pilaProvider():array {
+        return [
+            [[3, 4, 1, 8], 4],
+            [[], 0],
+            [[7], 1]
+        ];
+    }
+
 }
